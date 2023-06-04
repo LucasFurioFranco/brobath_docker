86b7b1917b32507b2ada9a38d81f9b318636b962
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-06-04 00:33:21 +0000

    quebrou?

---
+++ load.sh
@@ -0,0 +1,17 @@
+#Clones all other repositories of this project
+
+#api
+git clone https://github.com/LucasFurioFranco/brobath_docker_api.git api
+
+#bootstrap
+git clone https://github.com/LucasFurioFranco/brobath_docker_bootstrap.git bootstrap
+
+#react
+git clone https://github.com/LucasFurioFranco/brobath_docker_react.git react
+
+#static (nginx)
+git clone https://github.com/LucasFurioFranco/brobath_docker_nginx.git static
+
+#www
+git clone https://github.com/LucasFurioFranco/brobath_docker_www.git www
+
